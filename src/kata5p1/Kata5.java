76cd0100e9fea3c43ec1b27463301864b015a113
Isac <isac.as0001@gmail.com>
2021-11-18 13:00:49 +0000

    Kata 5p1 versión 2

--- src/kata5p1/Kata5.java
+++ src/kata5p1/Kata5.java
@@ -1,9 +1,29 @@
 package kata5p1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Kata5 {
 
     public static void main(String[] args) {
-        SelectApp app = new SelectApp();
-        app.selectAll();
+        createNewTable();
+    }
+    
+    
+    public static void createNewTable() {
+        String url = "jdbc:sqlite:KATA5";
+        String sql = "CREATE TABLE IF NOT EXISTS EMAIL (\n"
+                + " id integer PRIMARY KEY AUTOINCREMENT,\n"
+                + " direccion text NOT NULL);";
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println("Tabla creada");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
