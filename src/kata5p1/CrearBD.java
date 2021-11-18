970e5c4fb24cb2974e8c178e278479999b164dee
Isac <isac.as0001@gmail.com>
2021-11-18 12:54:44 +0000

    Kata 5p1 versión 1

---
+++ src/kata5p1/CrearBD.java
@@ -0,0 +1,23 @@
+package kata5p1;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CrearBD {
+
+    public static void createNewDatabase(String fileName) {
+        String url = "jdbc:sqlite:" + fileName;
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("El driver es " + meta.getDriverName());
+                System.out.println("Se ha creado una nueva BD.");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
---
+++ src/kata5p1/Kata5.java
@@ -0,0 +1,9 @@
+package kata5p1;
+
+public class Kata5 {
+
+    public static void main(String[] args) {
+        SelectApp app = new SelectApp();
+        app.selectAll();
+    }
+}
